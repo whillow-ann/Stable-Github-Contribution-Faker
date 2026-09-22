6a942c6cd72506f6893f7645b3d1db9efcde1b5f
whillow-ann <165192062+whillow-ann@users.noreply.github.com>
2026-09-22 20:40:03 +0000

    Auto contribution 1

--- rbq.java
+++ rbq.java
@@ -4,40 +4,16 @@ import java.io.IOException;
 
 public class rbq{
     public static void main() {
-      System.out.println("Rizz gyatt rage-baiting KYS");
-      System.out.println("goofy ahh GOAT jestermaxxing frfr");
-      System.out.println("UWU toliet touching grass bruh");
-      System.out.println("Rizz sussy baka is joever no cap");
-      System.out.println("goofy ahh balls vibe check no cap");
-      System.out.println("Gigachad sussy baka fanum taxed caught in 4K");
-      System.out.println("UWU balls simp W");
-      System.out.println("UWU bussi locked in W");
-      System.out.println("goofy ahh toliet rage-baiting caught in 4K");
-      System.out.println("Gigachad GOAT is joever twin");
-      System.out.println("UWU dih moged ratio");
-      System.out.println("Gigachad sussy baka  lowkirksixsevenuinely moged caught in 4K");
-      System.out.println("bussin' aura locked in no cap");
-      System.out.println("sigma bussi sus L");
-      System.out.println("Rizz sussy baka SDIYBTing bruh");
-      System.out.println("Gigachad dih gooning frfr");
-      System.out.println("Rizz aura simp caught in 4K");
-      System.out.println("Omega toliet be yappin' frfr");
-      System.out.println("Gigachad 67 vibe check KYS");
-      System.out.println("Skibidi bussi be yappin' L");
-      System.out.println("bussin' toliet gooning frfr");
-      System.out.println("UWU balls sus W");
-      System.out.println("Gigachad toliet sus KYS");
-      System.out.println("Omega toliet touching grass no cap");
-      System.out.println("Skibidi aura is joever ratio");
-      System.out.println("goofy ahh GOAT be yappin' frfr");
-      System.out.println("bussin' gyatt sus ratio");
-      System.out.println("UWU 67 jestermaxxing cooked");
-      System.out.println("Skibidi fire jestermaxxing ratio");
-      System.out.println("sigma balls is joever KYS");
-      System.out.println("Rizz aura looksmaxxing KYS");
-      System.out.println("goofy ahh fire touching grass no cap");
-      System.out.println("Cringe sussy baka washed W");
-      System.out.println("Rizz fire  lowkirksixsevenuinely moged bruh");
-      System.out.println("Skibidi fire sus twin");
+      System.out.println("Zesty fire rage-baiting bruh");
+      System.out.println("sigma dih rage-baiting L");
+      System.out.println("Omega bussi slapsW");
+      System.out.println("Gigachad drip rage-baiting moged ");
+      System.out.println("SYBAU drip SDIYBTing caught in 4K");
+      System.out.println("Gigachad bussi  lowkirksixsevenuinely moged cooked");
+      System.out.println("Gigachad sussy baka  lowkirksixsevenuinely moged W");
+      System.out.println("bussin' dih SDIYBTing no shit");
+      System.out.println("Zesty aura rage-baiting twin");
+      System.out.println("Zesty aura slapsfrfr");
+      System.out.println("Omega GOAT touching grass ratio");
   }
 }
